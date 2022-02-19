0582269407ef722f4874398dd507bfd4f5534417
Silvio Mori Neto <silviomori@gmail.com>
2022-02-18 21:32:11 -0800

    Use react-select in the filter component

--- front-web/src/components/filter/index.tsx
+++ front-web/src/components/filter/index.tsx
@@ -3,9 +3,16 @@ import { useEffect, useState } from 'react';
 import { Store } from 'types/store';
 import { requestBackend } from 'util/requests';
 import { AxiosRequestConfig } from 'axios';
+import { Controller, useForm } from 'react-hook-form';
+import Select from 'react-select';
+
+type FilterData = {
+  store: Store;
+};
 
 function Filter() {
   const [stores, setStores] = useState<Store[]>([]);
+  const { control } = useForm<FilterData>();
 
   useEffect(() => {
     const config: AxiosRequestConfig = {
@@ -20,7 +27,23 @@ function Filter() {
 
   return (
     <div className="base-card filter-container">
-      <input className="filter-input" value="Araguari"></input>
+      <div className="filter-select-container">
+        <Controller
+          name="store"
+          control={control}
+          render={({ field }) => (
+            <Select
+              {...field}
+              className="filter-select"
+              classNamePrefix="filter-select"
+              options={stores}
+              isClearable
+              getOptionLabel={(store: Store) => store.name}
+              getOptionValue={(store: Store) => String(store.id)}
+            />
+          )}
+        />{' '}
+      </div>
     </div>
   );
 }
