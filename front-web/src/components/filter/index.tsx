64b514bb501d294062b4dff64cc73de99bb5ea6f
Silvio Mori Neto <silviomori@gmail.com>
2022-02-18 20:42:30 -0800

    Retrieve list of stores from the backend

--- front-web/src/components/filter/index.tsx
+++ front-web/src/components/filter/index.tsx
@@ -1,6 +1,23 @@
 import './styles.css';
+import { useEffect, useState } from 'react';
+import { Store } from 'types/store';
+import { requestBackend } from 'util/requests';
+import { AxiosRequestConfig } from 'axios';
 
 function Filter() {
+  const [stores, setStores] = useState<Store[]>([]);
+
+  useEffect(() => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/stores'
+    };
+    requestBackend(config).then((response) => {
+      console.log(response.data);
+      setStores(response.data);
+    });
+  }, []);
+
   return (
     <div className="base-card filter-container">
       <input className="filter-input" value="Araguari"></input>
