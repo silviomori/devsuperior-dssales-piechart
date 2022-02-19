919d77fd59568f57390018121fbf31282f33ecff
Silvio Mori Neto <silviomori@gmail.com>
2022-02-18 18:00:41 -0800

    Create the filter component skeleton

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,17 @@
 import './assets/styles/custom.scss';
 import './App.css';
 import Navbar from 'components/navbar';
+import Filter from 'components/filter';
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Filter />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ front-web/src/components/filter/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+function Filter() {
+  return (
+    <div className="base-card filter-container">
+      <input className="filter-input" value="Araguari"></input>
+    </div>
+  );
+}
+
+export default Filter;
