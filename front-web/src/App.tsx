dacc9d6b867f1ac7756c8c248e2d26ffef6714ef
Silvio Mori Neto <silviomori@gmail.com>
2022-02-22 16:28:49 -0800

    Propagate filter changes to the dashboard

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,15 +2,22 @@ import './assets/styles/custom.scss';
 import './App.css';
 import Navbar from 'components/navbar';
 import Filter from 'components/filter';
+import { FilterData } from 'types/filter-data';
 import Dashboard from 'components/dashboard';
+import { useState } from 'react';
 
 function App() {
+  const [filterData, setFilterData] = useState<FilterData>();
+  const onFilterChange = (filter: FilterData) => {
+    setFilterData(filter);
+  };
+
   return (
     <>
       <Navbar />
       <div className="container">
-        <Filter />
-        <Dashboard />
+        <Filter onFilterChange={onFilterChange} />
+        <Dashboard filterData={filterData} />
       </div>
     </>
   );
--- front-web/src/components/dashboard/index.tsx
+++ front-web/src/components/dashboard/index.tsx
@@ -1,8 +1,18 @@
 import './styles.css';
 import TotalSales from './total-sales';
 import PieChart from './pie-chart';
+import { FilterData } from 'types/filter-data';
+import { useEffect } from 'react';
 
-function Dashboard() {
+type Props = {
+  filterData?: FilterData;
+};
+
+function Dashboard({ filterData }: Props) {
+  useEffect(() => {
+    console.log('Dashboard:', filterData);
+  }, [filterData]);
+
   return (
     <div className="base-card dashboard-container">
       <div className="dashboard-total-sales">
